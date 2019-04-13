19cf65c327cb3bc1d51135b7daac2ab3d77fcc5a
Jitendra <kumar.jitendra0201@gmail.com>
2019-04-13 11:10:54 +0530

    updated

--- complete/src/main/java/hello/MvcConfig.java
+++ complete/src/main/java/hello/MvcConfig.java
@@ -1,17 +1,27 @@
 package hello;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
 
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/home").setViewName("home");
-        registry.addViewController("/").setViewName("home");
-        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/").setViewName("login");
         registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/hello").setViewName("hello");
     }
 
+    @Bean
+    public ViewResolver viewResolver () {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/templates/");
+        viewResolver.setSuffix(".html");
+        return viewResolver;
+    }
 }
--- complete/src/main/java/hello/WebSecurityConfig.java
+++ complete/src/main/java/hello/WebSecurityConfig.java
@@ -17,11 +17,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-                .antMatchers("/", "/home").permitAll()
+                .antMatchers("/").permitAll()
                 .anyRequest().authenticated()
                 .and()
             .formLogin()
-                .loginPage("/login")
+                .loginPage("/login").defaultSuccessUrl("/hello")
                 .permitAll()
                 .and()
             .logout()
